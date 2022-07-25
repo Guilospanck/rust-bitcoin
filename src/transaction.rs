666d03e2597c230dd191e888fcfa66342afb7594
Guilherme Rodrigues <guilherme.rp2010@hotmail.com>
2022-07-25 20:46:42 +0100

    docs: vin serialize doc

--- src/transaction.rs
+++ src/transaction.rs
@@ -69,6 +69,20 @@ impl Vin {
     }
   }
 
+  /// Serializes a Vin.
+  /// 
+  /// ```rust
+  /// let mut vin = transaction::Vin::new();
+  /// vin.txid = "7957a35fe64f80d234d76d83a2a8f1a0d8149a41d81de548f0a65a8a999f6f18".to_owned();
+  /// vin.vout = 0;
+  /// vin.script_sig = "483045022100884d142d86652a3f47ba4746ec719bbfbd040a570b1deccbb6498c75c4ae24cb02204b9f039ff08df09cbe9f6addac960298cad530a863ea8f53982c09db8f6e381301410484ecc0d46f1918b30928fa0e4ed99f16a0fb4fde0735e7ade8416ab9fe423cc5412336376789d172787ec3457eee41c04f4938de5cc17b4a10fa336a8d752adf".to_owned();
+  /// vin.sequence = 4294967295;
+  /// 
+  /// let serialized = vin.serialize();
+  /// let expected = "186f9f998a5aa6f048e51dd8419a14d8a0f1a8a2836dd734d2804fe65fa35779000000008b483045022100884d142d86652a3f47ba4746ec719bbfbd040a570b1deccbb6498c75c4ae24cb02204b9f039ff08df09cbe9f6addac960298cad530a863ea8f53982c09db8f6e381301410484ecc0d46f1918b30928fa0e4ed99f16a0fb4fde0735e7ade8416ab9fe423cc5412336376789d172787ec3457eee41c04f4938de5cc17b4a10fa336a8d752adfffffffff".to_owned();
+  /// assert_eq!(serialized, expected);
+  /// ```
+  /// 
   pub fn serialize(&self) -> String {
     let txid_in_le_bytes_form = helpers::hex_to_le_bytes(self.txid.clone());
     let vout_4_bytes = hex::encode(self.vout.to_be_bytes());
